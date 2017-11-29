dd30ca2b4b98b8b91579a8ab394ed1352b4178d8
Michael Heydt <mike@heydt.org>
2017-11-29 15:33:46 -0700

    More Roles for .NET

--- Deploy-Vmss/CSE/install-and-configure-iis.ps1
+++ Deploy-Vmss/CSE/install-and-configure-iis.ps1
@@ -1,8 +1,7 @@
 #
 # install_and_configure_iis.ps1
 #
-Install-WindowsFeature Web-Server
-Install-WindowsFeature Web-Asp-Net45
+Install-WindowsFeature Web-Server, Web-Asp-Net45, NET-Framework-Features
 Install-WindowsFeature Web-Mgmt-Console
 
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
