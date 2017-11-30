10f83c5dcd4016d383a27f368d44b95e0856832b
Michael Heydt <mike@heydt.org>
2017-11-30 12:01:04 -0700

    Install .net 4.5

--- Deploy-Vmss/CSE/install-and-configure-iis.ps1
+++ Deploy-Vmss/CSE/install-and-configure-iis.ps1
@@ -1,6 +1,7 @@
 #
 # install_and_configure_iis.ps1
 #
+Install-WindowsFeature Net-Framework-45-Features
 Install-WindowsFeature Web-Server, Web-Asp-Net45, NET-Framework-Features
 Install-WindowsFeature Web-Mgmt-Console
 
