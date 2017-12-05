29630da0ff165dfda1584a637aa442260c080aaa
Michael Heydt <mike@heydt.org>
2017-12-05 12:16:52 -0700

    Create user for IIS

--- Deploy-Vmss/CSE/install-and-configure-iis.ps1
+++ Deploy-Vmss/CSE/install-and-configure-iis.ps1
@@ -1,6 +1,11 @@
 #
 # install_and_configure_iis.ps1
 #
+
+$username = "stgfiles1wspri"
+$password = ConvertTo-SecureString "5NZYMJoyyrQLoEwcxGBlgDBdLMrnWsCx4BEwfsSdkM33QzgsmuXadz5gr3j8PIeBfeg1E9XqD473j9Nk2QYsRg=="
+New-LocalUser -Name $username -Password $password -PasswordNeverExpires -UserMayNotChangePassword -AccountNeverExpires
+
 Install-WindowsFeature Net-Framework-45-Features
 Install-WindowsFeature Web-Server, Web-Asp-Net45, NET-Framework-Features
 Install-WindowsFeature Web-Mgmt-Console
