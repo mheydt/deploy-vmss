c57b9059d9159d7057d22072f034c74ff677b10c
Michael Heydt <mike@heydt.org>
2017-12-10 21:30:16 -0700

    Jump server config

---
+++ Deploy-Vmss/CSE/configure-jump-server.ps1
@@ -0,0 +1,42 @@
+#
+# configure_jump_server.ps1
+#
+
+Function Write-Log
+{
+   Param ([string]$logstring)
+
+   Add-Content -Path "c:\configure.log" -Value $logstring
+} 
+
+Try
+{
+	$storageAccountKey = "463znlo22viNBm3ACyTCeHaZJDqHkCY6SH9oLMIv3yVr/7RzXphZKS2KCZxJ4eLwQkWThK2wBwXo42pHHkNDdw=="
+	$storageAccountName = "stginstallerswspri"
+	$containerName = "sqlserver"
+	$ssmsInstallBlobName = "SSMS-Setup-ENU.exe"
+	$destinationSSMS = "d:\SSMS-Setup-ENU.exe"
+
+	Write-Log "Retriving SSMS"
+
+	$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+	Get-AzureStorageBlobContent -Blob $ssmsInstallBlobName -Container $containerName -Destination $destinationSSMS -Context $storageContext
+
+	Write-Log "Retrieved SSMS"
+	
+	Write-Log "Installing SSMS"
+	Start-Process "d:\SSMS-Setup-ENU.exe" "/install /quiet /norestart /log d:\ssms-log.txt" -Wait
+	Write-Log "Installed SSMS"
+
+	Write-Log "Cleaning up"
+	Remove-Item -Path $destinationSSMS
+    Remove-Item -Path d:\log*.txt
+    Remove-Item -Path d:\ssms-*.txt
+
+	Write-Log "All done!"
+}
+Catch
+{
+	Write-Log "Exception"
+	Write-Log $_.Exception.Message
+}
