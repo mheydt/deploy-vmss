660449fbc3ee8ccd8ab584f1194182f96bae8eb2
Michael Heydt <mike@heydt.org>
2017-12-20 16:26:39 -0700

    Added script to configure FTP

---
+++ Deploy-Vmss/CSE/configure-ftp-server.ps1
@@ -0,0 +1,28 @@
+Function Write-Log
+{
+	Param ([string]$logstring)
+
+	$Logfile = "c:\config.log"
+	Add-content $Logfile -value $logstring
+}
+
+Write-Log "Trusting PSGallery"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Write-Log("Starting installation of IIS")
+
+Write-Log('Creating local use to access AZF')
+$username = "stgfiles1wspdpr"
+$password = ConvertTo-SecureString -String "L0kVane0WMkH+6PP9rpEcoUkkFCT75bJBBNtuKayyFWS+DOtARL9bofx8fBKBZ9faHFpqTT5PxTH+PowJte7rQ==" -AsPlainText -Force
+New-LocalUser -Name $username -Password $password -PasswordNeverExpires -UserMayNotChangePassword -AccountNeverExpires
+Write-Log('User created')
+
+Write-Log('Configuring .NET 4.5')
+Install-WindowsFeature Net-Framework-45-Features
+Write-Log('Configuring Web Server, ASP.NET 4.5')
+Install-WindowsFeature Web-Ftp-Server, NET-Framework-Features
+Write-Log("Installing management console")
+Install-WindowsFeature Web-Mgmt-Console
+
+Write-Log('IIS config complete')
